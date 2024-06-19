7b7c43007785638be695c5578ae6c6fcb458bb6f
cory0105 <161662652+cory0105@users.noreply.github.com>
2024-06-20 08:58:28 +0900

    0620

--- PTR/PTR/src/main/resources/static/js/mypage.js
+++ PTR/PTR/src/main/resources/static/js/mypage.js
@@ -1,3 +1,138 @@
+const user = {
+    userId: "cake"
+}
+
+axios
+.post("http://localhost:8080/mySubscription", user)
+.then((response)=>{
+    console.log("데이터: ", response.data)
+    createMySubscription(response.data)   
+})
+.catch((error)=>{
+    console.log("에러: ", error)
+})
+
+function createMySubscription(data){
+    const subscriptionBody = document.querySelector(".content_body_subscriptionlist")
+    
+    data.forEach((data, index)=>{
+        const box = document.createElement("div");
+        box.classList.add("content_body_subscriptionlist_box");
+        box.classList.add("box");
+
+        const image = document.createElement("div");
+        const img = document.createElement("img");
+        image.classList.add("content_body_subscriptionlist_box_img");
+        img.src = data.teacher.user.profileImg;
+        
+        const name = document.createElement("div");
+        name.classList.add("content_body_subscriptionlist_box_name");
+        name.textContent = data.teacher.user.username;
+
+        const butten = document.createElement("div");
+        butten.classList.add("content_body_subscriptionlist_box_butten");
+        butten.textContent = "자세히보기"
+
+        image.appendChild(img);
+        box.appendChild(image);
+        box.appendChild(name);
+        box.appendChild(butten);
+        subscriptionBody.appendChild(box);
+    })
+}
+
+axios
+.post("http://localhost:8080/myBuyLecture", user)
+.then((response)=>{
+    console.log("데이터: ", response.data)
+    createMyLecture(response.data) 
+})
+.catch((error)=>{
+    console.log("에러: ", error)
+})
+
+
+
+
+
+{/* <div class="content_body_favoriteLecture">
+              <div class="content_body_favoriteLecture_box box">
+                <div class="content_body_favoriteLecture_box_img"></div>
+                <div class="content_body_favoriteLecture_box_title">제목</div>
+              </div>
+              <div class="content_body_favoriteLecture_box box">
+                <div class="content_body_favoriteLecture_box_img"></div>
+                <div class="content_body_favoriteLecture_box_title">제목</div>
+              </div>
+              <div class="content_body_favoriteLecture_box box">
+                <div class="content_body_favoriteLecture_box_img"></div>
+                <div class="content_body_favoriteLecture_box_title">제목</div>
+              </div>
+              <div class="content_body_favoriteLecture_box box">
+                <div class="content_body_favoriteLecture_box_img"></div>
+                <div class="content_body_favoriteLecture_box_title">제목</div>
+              </div>
+              <div class="content_body_favoriteLecture_box box">
+                <div class="content_body_favoriteLecture_box_img"></div>
+                <div class="content_body_favoriteLecture_box_title">제목</div>
+              </div>
+              <div class="content_body_favoriteLecture_box box">
+                <div class="content_body_favoriteLecture_box_img"></div>
+                <div class="content_body_favoriteLecture_box_title">제목</div>
+              </div>
+            </div> */}
+
+function createMyLecture(data){
+    const lectureBody = document.querySelector(".content_body_favoriteLecture");
+
+    data.forEach((data, index)=>{
+        const box = document.createElement("div");
+        box.classList.add("content_body_favoriteLecture_box");
+        box.classList.add("box");
+
+        const image = document.createElement("div");
+        const img = document.createElement("video");
+        image.classList.add("content_body_favoriteLecture_box_img");
+        img.src = data.lecture.url;
+        
+        const name = document.createElement("div");
+        name.classList.add("content_body_favoriteLecture_box_title");
+        name.textContent = data.lecture.lectureName;
+
+        
+        image.appendChild(img);
+        box.appendChild(image);
+        box.appendChild(name);
+        lectureBody.appendChild(box);
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 document.querySelector(".subscription").addEventListener("click", ()=>{
     document.querySelector(".content_body_subscription").classList.remove("hiden")
