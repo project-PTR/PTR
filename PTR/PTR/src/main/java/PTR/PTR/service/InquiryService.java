deebaf26d7c33410dd3491ab7238fada01c51184
ardnim <dudah1019@naver.com>
2024-06-27 19:32:55 +0900

    InquiryService fixed

--- PTR/PTR/src/main/java/PTR/PTR/service/InquiryService.java
+++ PTR/PTR/src/main/java/PTR/PTR/service/InquiryService.java
@@ -1,12 +1,16 @@
 package PTR.PTR.service;
 
 import PTR.PTR.model.Inquiry;
+import PTR.PTR.model.InquiryReply;
 import PTR.PTR.model.User;
+import PTR.PTR.repository.InquiryReplyRepository;
 import PTR.PTR.repository.InquiryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,10 +18,13 @@ import java.util.stream.Collectors;
 public class InquiryService {
     InquiryRepository inquiryRepository;
     InquiryReplyService inquiryReplyService;
+    InquiryReplyRepository inquiryReplyRepository;
 
-    public InquiryService(InquiryRepository inquiryRepository, InquiryReplyService inquiryReplyService) {
+    public InquiryService(InquiryRepository inquiryRepository, InquiryReplyService inquiryReplyService,
+                          InquiryReplyRepository inquiryReplyRepository) {
         this.inquiryRepository = inquiryRepository;
         this.inquiryReplyService = inquiryReplyService;
+        this.inquiryReplyRepository = inquiryReplyRepository;
     }
 
     public Inquiry askInquiry(Inquiry inquiry){
@@ -26,7 +33,18 @@ public class InquiryService {
     }
 
     public List<Inquiry> findInquiry(){
-        return inquiryRepository.findAll();
+        List<Inquiry> inquiryList = inquiryRepository.findAll();
+        List<Inquiry> newInquiryList = new ArrayList<>();
+        newInquiryList = inquiryList.stream().map((data)->{
+            List<InquiryReply> inquiryReplyList = inquiryReplyRepository.findByInquiry(data);
+            if (inquiryReplyList.size() > 0) {
+                data.setTitle(data.getTitle() + " [답변완료]");
+                data.setText(data.getText() + "\n\n[답변]\n"
+                        + inquiryReplyList.getFirst().getText());
+            }
+            return data;
+        }).collect(Collectors.toList());
+        return newInquiryList;
     }
 
     public List<Inquiry> myInquiry(User user){
